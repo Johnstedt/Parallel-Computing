f7464db448a0bad281ccdf29cf33a4c78d4a4ded
John-John Markstedt <c14jmt@cs.umu.se>
2018-02-06 17:13:49 +0100

    submatrices does actually work now

--- MPI/vector_type.c
+++ MPI/vector_type.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-void send_vector(int count, int blocklenght, int stride, int* matrix, int send_to);
-void receive_vector(int count, int blocklenght, int stride, int* matrix, int receive_from);
+void send_vector(int x, int y, int rows, int columns, int* matrix, int send_to);
+void receive_vector(int x, int y, int rows, int columns, int* matrix, int receive_from);
 
 int main(void) {
 
@@ -25,37 +25,46 @@ int main(void) {
 		for(int i = 0; i < 36; i++){
 			matrix[i] = i;
 		}
-		send_vector(6, 3, 6, matrix, 1);
+		send_vector(4,4,2,2, matrix, 1);
 
 	} else if(my_rank == 1){
 
-		receive_vector(6, 3, 6, matrix, 0);
+		receive_vector(4,4,2,2, matrix, 0);
+		for (int i = 0; i < 36; i++) {
+			printf("%d, ", matrix[i]);
+
+        	if((i+1) % 6 == 0 ){
+        		printf("\n");
+        	}
+    	}
 	}
 
 	MPI_Finalize();
 	return 0;
 }
 
-void send_vector(int count, int blocklenght, int stride, int* matrix, int send_to){
+void send_vector(int x, int y, int rows, int columns, int* matrix, int send_to){
+
+	int count = columns;
+	int blocklenght = rows;
+	int stride = 6;
 
 	MPI_Datatype new_type;
 	MPI_Type_vector(count, blocklenght, stride, MPI_INT, &new_type);
 	MPI_Type_commit(&new_type);
 
-	MPI_Send(matrix, 1, new_type, send_to, 0, MPI_COMM_WORLD);	
+	MPI_Send(&matrix[(x*6) + y], 1, new_type, send_to, 0, MPI_COMM_WORLD);	
 }
 
-void receive_vector(int count, int blocklenght, int stride, int* matrix, int receive_from){
+void receive_vector(int x, int y, int rows, int columns, int* matrix, int receive_from){
+
+	int count = columns;
+	int blocklenght = rows;
+	int stride = 6;
 
 	MPI_Datatype new_type;
 	MPI_Type_vector(count, blocklenght, stride, MPI_INT, &new_type);
 	MPI_Type_commit(&new_type);
 
-	MPI_Recv(matrix, 1, new_type, receive_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);	
-
-/* Loop through the array it was received into, printing values */
-    for (int i = 0; i < 36; i++)
-    {
-        printf("%d\n", matrix[i]);
-    }
+	MPI_Recv(&matrix[(x*6) + y], 1, new_type, receive_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);	
 }
